878017627b322bd032c062a18937d520b57d6ce2
kur0byte <santiagozapatabohorquez@hotmail.com>
2024-12-06 10:33:19 -0500

    feat: added express server example

---
+++ example/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { correlationMw } = require('../lib/correlationIdMiddleware');
+
+const app = express();
+
+const server = () => {
+    app.use(correlationMw());
+    app.get('*', (req, res, next) => {
+        console.log({Request: req.url, CorrelationId: res.get('id-hly')});
+        console.log(`Correlation ID can be accessed within the request handler: ${req.correlationId()}`);
+        console.log(`Correlation ID can be accessed within the response header: ${res.get('id-hly')}`);
+        
+        
+        res.on('finish', () => {
+            console.log(`ID within finish is ${req.correlationId()}`);
+        });
+
+        res.send(`Hello World!`);
+    });
+
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+    
+    app.listen(3000, () => {
+        console.log('Server started on http://localhost:3000');
+    });
+}
+
+module.exports = { server };
